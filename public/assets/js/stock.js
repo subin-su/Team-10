07fc2e48d5bc42aa40ba58c72e74228c0e6c3916
Subin <subinkc140@gmail.com>
2021-03-21 00:08:08 -0700

    completed

--- public/assets/js/stock.js
+++ public/assets/js/stock.js
@@ -5,10 +5,11 @@ var val=document.getElementById("stock");
 var cab=document.getElementById("abc")
 var moment1=document.getElementById("moment")
 moment1.innerHTML=moment().format('LLLL');
+var delete1=document.getElementById('delete')
 
 val.addEventListener('click', function () {
     // Sweet Alert
-   
+    
     
     fetch(`/api/stockSearch/${cab.value}`, {
         method: "PUT",
@@ -26,26 +27,59 @@ val.addEventListener('click', function () {
             // Set Variable for the stock symbol
             let stockSymbol = data["Global Quote"]["01. symbol"]
             // // Set Variable for the stock price
+            let stockhigh = data["Global Quote"]["03. high"]
+            let stocklow = data["Global Quote"]["04. low"]
+            // // Set Variable for the stock price
             let stockPrice = data["Global Quote"]["05. price"]
-
-           // Query selector the classes
-            let company = document.querySelector("#company")
-            let price = document.querySelector("#price")
-            // Display the search results to the page
-            company.innerHTML = (`Stock Symbol: ${stockSymbol}`)
-            price.textContent = (`Price: $${stockPrice}`)
-            let condiments = document.querySelector("#condiments ")
-            // Display the search results to the page
-            var btn1 = document.createElement("div");
             
-            btn1.innerHTML=cab.value;
-            var deleteButton = document.createElement('button');
-  deleteButton.textContent = 'delete';
-            condiments.appendChild(btn1)
-            condiments.appendChild(deleteButton)
+            if(stockSymbol){
+                let company = document.querySelector("#company")
+                let price = document.querySelector("#price")
+                let high = document.querySelector("#high")
+                let low = document.querySelector("#low")
+                // Display the search results to the page
+                company.innerHTML = (`Stock Symbol: ${stockSymbol}`)
+                price.textContent = (`Price: $${stockPrice}`)
+                high.innerHTML=(`High:${stockhigh}`)
+                low.innerHTML=(`High:${stocklow}`)
+                let condiments = document.querySelector("#condiments ")
+                // Display the search results to the page
+                var btn1 = document.createElement("div");
+                
+                btn1.innerHTML=cab.value;
+                localStorage.setItem("Symbol" ,stockSymbol)
+                localStorage.setItem("price" ,stockPrice)
+                swal("You Searched for",`${stockSymbol}`, "success");
+            }else{
+                swal(`You Searched for ${cab.value}`,"Please enter valid ticker Symbol", "error");
+                return;
+            }
+           // Query selector the classes
+           
+  
 
         })
 
+    })
+    delete1.addEventListener('click',function(){
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, Data will be gone",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+                company.innerHTML=" ";
+                price.innerHTML=""
+                high.innerHTML =""
+                low.innerHTML=""
+            } else {
+              swal("Your imaginary file is safe!");
+            }
+          });
+      
     })
 
 })
